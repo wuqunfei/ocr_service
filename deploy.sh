b3346a45a891ea95e91969fc280116d9970b7783
Qunfei Wu <qunfei.wu@allianz.com>
2022-03-15 11:56:30 +0100

    add run scripts

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 helm upgrade --install pysimple ./helm \
 -n customer-test \
+--set nameOverride=pysimple \
 --set image.repository=proddstraining62so.azurecr.io/pysimple \
---set image.tag=13 \
+--set image.tag=14 \
 --set ingress.enable=true \
 --set "ingress.hosts[0].host=pysimple.gdp-09.we1.azure.aztec.cloud.allianz,ingress.hosts[0].paths[0].path=/,ingress.hosts[0].paths[0].pathType=ImplementationSpecific"
-
-
