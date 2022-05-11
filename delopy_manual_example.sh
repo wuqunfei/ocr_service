74a09c4940bde8f540e83b305181c934614a3066
Qunfei Wu <wu.qunfei@gmail.com>
2022-05-11 18:51:32 +0200

    Delete delopy_manual_example.sh

--- delopy_manual_example.sh
+++
@@ -1,7 +0,0 @@
-helm upgrade --install pysimple ./helm \
--n customer-test \
---set nameOverride=pysimple \
---set image.repository=proddstraining62so.azurecr.io/pysimple \
---set image.tag=14 \
---set ingress.enable=true \
---set "ingress.hosts[0].host=pysimple.gdp-09.we1.azure.aztec.cloud.allianz,ingress.hosts[0].paths[0].path=/,ingress.hosts[0].paths[0].pathType=ImplementationSpecific"
